be07ed5be04cd823722176765eb6009aa09a3f1a
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2023-07-13 19:04:17 +0530

    Update start.py

--- start.py
+++ start.py
@@ -10,19 +10,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 SCROLL_PAUSE_TIME = 1.0
 
-# options = Options()
-# options.set_headless(headless = False)
-
-# Workaround since latest geckodriver and Firefox versions do not completely support Selenium
-# capabilities = webdriver.DesiredCapabilities().FIREFOX
-# capabilities["marionette"] = True
-
-# firefox_profile = FirefoxProfile()
-# ## Disable images
-# firefox_profile.set_preference('permissions.default.image', 2)
 CHROMEDRIVER_PATH = r'/Volumes/fineanmol/Python/Bots/chromedriver'
-driver = webdriver.Chrome(CHROMEDRIVER_PATH)
-# cookies
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # Uncomment this line to run in headless mode
+driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
 
 cookies_file_path = '/Volumes/fineanmol/Python/Bots/LinkedIn-2/cookies.pkl'
 
@@ -30,9 +22,6 @@ cookies_file_path = '/Volumes/fineanmol/Python/Bots/LinkedIn-2/cookies.pkl'
 def save_cookie(path):
     with open(path, 'wb') as filehandler:
         pickle.dump(driver.get_cookies(), filehandler)
-
-
-driver.get("https://www.linkedin.com")
 
 
 def load_cookie(path):
@@ -43,19 +32,13 @@ def load_cookie(path):
                 driver.add_cookie(cookie)
     except Exception as e:
         print(e)
-        #replace  empty string [""] with your username and password of Linkedin Account.
+        # replace empty string [""] with your username and password of Linkedin Account.
         username = ""
         password = ""
         driver.find_element_by_id('session_key').send_keys(username)
         driver.find_element_by_id('session_password').send_keys(password)
-        driver.find_element_by_class_name(
-            'sign-in-form__submit-button').click()
+        driver.find_element_by_class_name('sign-in-form__submit-button').click()
         save_cookie(path)
-
-
-load_cookie(cookies_file_path)
-
-driver.get("https://www.linkedin.com/mynetwork/invite-connect/connections")
 
 
 def scrollToBottom():
@@ -88,39 +71,44 @@ def scrollToBottom():
         last_height = new_height
 
 
-scrollToBottom()
-cards = driver.find_elements_by_class_name('mn-connection-card__link')
-print(len(cards))
-links = []
+def main():
+    load_cookie(cookies_file_path)
 
-for card in cards:
-    links.append(card.get_attribute('href'))
-with open("connections.csv" + time.asctime(time.localtime(time.time())), "w", newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=[
-        'Link'])
-    writer.writeheader()
+    driver.get("https://www.linkedin.com/mynetwork/invite-connect/connections")
+    scrollToBottom()
 
-    bar = IncrementalBar('Fetching Connections', max=len(links))
-    print(len(links))
-    for link in links:
+    cards = driver.find_elements_by_class_name('mn-connection-card__link')
+    print(len(cards))
+    links = [card.get_attribute('href') for card in cards]
 
-        driver.get(link + "details/skills/")
-        time.sleep(SCROLL_PAUSE_TIME)
-        try:
-            elements = driver.find_elements_by_xpath("//span[text()='Endorse']")
+    with open("connections.csv" + time.asctime(time.localtime(time.time())), "w", newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['Link'])
+        writer.writeheader()
+
+        bar = IncrementalBar('Fetching Connections', max=len(links))
+        print(len(links))
+        for link in links:
+            driver.get(link + "details/skills/")
+            time.sleep(SCROLL_PAUSE_TIME)
             try:
-                endorsedElement = driver.find_element_by_xpath("//span[text()='Endorsed']")
-                driver.execute_script("arguments[0].scrollIntoView();", endorsedElement)
+                elements = driver.find_elements_by_xpath("//span[text()='Endorse']")
+                try:
+                    endorsedElement = driver.find_element_by_xpath("//span[text()='Endorsed']")
+                    driver.execute_script("arguments[0].scrollIntoView();", endorsedElement)
+                except Exception as e:
+                    print(e, "no skills endorsed yet")
+                    pass
+                for element in elements:
+                    element.click()
             except Exception as e:
-                print(e, "no skills endorsed yet")
+                print(e, "no skills left to endorse")
                 pass
-            for element in elements:
-                element.click()
-        except Exception as e:
-            print(e, "no skills left to endorse")
-            pass
-        writer.writerow({'Link': link})
-        bar.next()
+            writer.writerow({'Link': link})
+            bar.next()
 
-    bar.finish()
-    driver.close()
+        bar.finish()
+        driver.close()
+
+
+if __name__ == "__main__":
+    main()
